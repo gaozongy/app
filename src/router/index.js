3044d418aacd433cbbd3ea1d96345739516d6abe
GaozyDev <670453367@qq.com>
2022-04-24 13:56:08 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -14,21 +14,25 @@ export default new VueRouter({
     routes:[
         {
             path:'/home',
-            component:Home
+            component:Home,
+            meta:{show:true}
         },
         {
             path:'/search',
-            component:Search
+            component:Search,
+            meta:{show:true}
         }
         ,
         {
             path:'/login',
-            component:Login
+            component:Login,
+            meta:{show:false}
         }
         ,
         {
             path:'/register',
-            component:Register
+            component:Register,
+            meta:{show:false}
         },
         //重定向，在项目跑起来的时候，访问/，立马让他定向到首页
         {
