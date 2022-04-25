290b28619d509eddd28ea0aadefb836dd7a41376
GaozyDev <670453367@qq.com>
2022-04-25 17:07:01 +0800

    完成Home静态组件的拆分

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -2,12 +2,34 @@
   <div>
   <!-- 使用三级联动全局组件，三级联动已经注册为全局组件，因此不需要再引入，直接使用即可 -->
     <TypeNav/>
+    <ListContainer/>
+    <TodayRecommend/>
+    <Rank/>
+    <Like/>
+    <Floor/>
+    <Floor/>
+    <Brand/>
   </div>
 </template>
 
 <script>
+//引入其余的组件
+import ListContainer from "@/pages/Home/ListContainer";
+import TodayRecommend from "@/pages/Home/TodayRecommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
 export default {
   name: '',
+  components:{
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
 }
 </script>
 
