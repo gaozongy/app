cc080ed5fb16766885a3f4d2fdb9bb4d7503b5c0
GaozyDev <670453367@qq.com>
2022-04-25 15:29:00 +0800

    完成Home模块三级联动全局组件

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+//三级联动组件---全局组件
+import TypeNav from "@/pages/Home/TypeNav";
+//第一个参数是：全局组件的名字  第二个参数是：哪一个组件
+Vue.component(TypeNav.name,TypeNav)
 //引入路由
 import router from '@/router'
 
