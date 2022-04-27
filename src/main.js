aaa05b563c61099501e515261050ecab27b665a5
GaozyDev <670453367@qq.com>
2022-04-27 23:54:30 +0800

    完成TypeNav三级联动展示数据业务

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 //三级联动组件---全局组件
-import TypeNav from "@/pages/Home/TypeNav";
+import TypeNav from "@/components/TypeNav";
 //第一个参数是：全局组件的名字  第二个参数是：哪一个组件
 Vue.component(TypeNav.name,TypeNav)
 //引入路由
 import router from '@/router'
 //引入仓库
 import store from './store'
-//测试一下
-import {reqCategoryList} from '@/api'
-reqCategoryList()
+
 
 Vue.config.productionTip = false
 
--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,7 +1,25 @@
+import {reqCategoryList} from "@/api";
 //home模块的小仓库
-const state = {}
-const mutations = {}
-const actions = {}
+const state = {
+    //state中数据默认初始值别瞎写，服务器返回的数据是对象，那么state中数据默认初始值就是对象；
+    //服务器返回的数据是数组，那么state中数据默认初始值就是数组
+    //state中数据默认初始值是根据接口返回值类型写的
+    categoryList:[]
+}
+const mutations = {
+    CATEGORYLIST(state,categoryList) {
+        state.categoryList = categoryList
+    }
+}
+const actions = {
+    //通过API里面的接口函数调用，向服务器发请求，获取服务器的数据
+    async categoryList({commit}) {
+        let result = await reqCategoryList()
+        if (result.code == 200) {
+            commit('CATEGORYLIST',result.data)
+        }
+    }
+}
 const getters = {}
 export default {
     state,
