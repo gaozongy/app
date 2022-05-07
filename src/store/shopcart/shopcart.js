a966ce52b4b74bacdbbd11d966769c2021e76b65
GaozyDev <670453367@qq.com>
2022-05-07 17:59:17 +0800

    购物车展示数据

--- src/store/shopcart/shopcart.js
+++ src/store/shopcart/shopcart.js
@@ -1,21 +1,26 @@
 import {reqCartList} from "@/api";
 
 const state = {
-
+    cartList:[]
 }
 const mutations = {
-
+    GETCARTLIST(state,cartList) {
+        state.cartList = cartList
+    }
 }
 const actions = {
     //获取购物车列表数据
     async getCartList({commit}) {
         let result = await reqCartList()
-        console.log(result)
+        if(result.code==200) {
+            commit('GETCARTLIST',result.data)
+        }
     }
 }
 const getters = {
-
-
+    cartList(state) {
+        return state.cartList[0] || {}
+    }
 }
 export default {
     state,
